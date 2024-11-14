884c81292c50a14935c45ee3c5c8152efe32aa6f
Amanda Rost <amandarostprog@gmail.com>
2024-11-14 16:09:06 -0300

    Iniciando o trabalho

---
+++ app.js
@@ -0,0 +1,22 @@
+const request = require('request')
+let express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/criarConta', (req, res) => {
+    res.render('criarConta')
+})
+app.listen(3000, () => console.log("Servidor ligado na porta 3000!"))
